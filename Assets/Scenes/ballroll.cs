a11948b3b2a86f235f2421aea77f3a43032dee74
CFrance <calebfrance005@gmail.com>
2024-03-27 23:21:05 +0200

    Game Files Addition
    Void-Runner ball movement and beginning level designs

---
+++ Assets/Scenes/ballroll.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballroll : MonoBehaviour
+{
+    public float speed;
+    
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb = GetComponent<Rigidbody>();
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        Vector3 roll = new Vector3(x:moveHorizontal, y:0, z:moveVertical);
+        Vector3 Jump = new Vector3(x:0, y:0, z: moveVertical);
+
+        rb.AddForce(roll * (speed * Time.deltaTime));
+        rb.AddForce( Jump * (speed * Time.deltaTime));
+    }
+}
